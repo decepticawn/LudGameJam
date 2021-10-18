63fa260e82dad813827ab01a29b9f8958f92ebc1
decepticawn <decepticawn@gmail.com>
2021-10-18 16:27:52 -0600

    Jump Mechanics
    Added jump mechanics and moved the camera a bit, had to do a couple of things differently from how it would work in 2d. Not sure how good it feels, but it works for now. Don't exactly know how to make it look like the player is charging the jump, probably would be through animation. :D

--- LudGame/Assets/Scripts/PlayerController.cs
+++ LudGame/Assets/Scripts/PlayerController.cs
@@ -7,34 +7,112 @@ public class PlayerController : MonoBehaviour
     //Public player movement variables
     public int speed;
     public int baseSpeed = 5;
-    public bool isGrounded;
     public float jumpHeight;
     public bool canJump = true;
     public float jumpValue = 0.0f;
+    public float realjumpValue = 0.0f;
+    public float isJump = 0;
 
-    //Ground mask to determine if player is grounded
-    public LayerMask groundMask;
+    //Variables used to check if the player is grounded
+    public float distanceGround;
+    public bool isGrounded  = false;
+
+    //Physics material
+    public PhysicMaterial playerMat;
+
+    //Gravity scale since unity 3d doesnt have one on rigidbodies
+    public float gravityScale = 4.0f;
+    public static float globalGravity = -9.81f;
 
     //Private player variables
     private Rigidbody rb;
+    private Collider coll;
     private float moveHorizontal;
     // Start is called before the first frame update
     void Start()
     {
         speed = baseSpeed;
+        distanceGround = GetComponent<Collider>().bounds.extents.y;
         rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        realjumpValue = jumpValue/10;
         //A/D <-/-> Movement
         moveHorizontal = Input.GetAxis("Horizontal");
         //How fast you move on ground
         rb.velocity = new Vector3(moveHorizontal * speed, rb.velocity.y, 0);
-        /*Determines if the player is grounded, need to maybe use raycasts because this is similar to how I did it in my 2d game but I don't think it'll work in 3d
-        isGrounded = Physics.OverlapBox(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f, gameObject.transform.position.z),
-        new Vector3(0.9f, 0.4f, 0f), Quaternion.identity, groundMask);
-        */
+        //Changes the physics material if the player is jumping
+        if(jumpValue > 0)
+        {
+            coll.material.bounciness = 0.5f;
+        }
+        else
+        {
+            coll.material.bounciness = 0f;
+        }
+
+        //Increases jump height based on how long you hold space
+        if(Input.GetButton("Jump") &&isGrounded&&canJump)
+        {   
+            isJump = 1;     
+            rb.velocity = new Vector3(0f, 0f, 0f); 
+            jumpValue += 0.1f;
+        }
+        if(Input.GetButtonDown("Jump")&& isGrounded && canJump)
+        {
+            isJump = 1;
+            rb.velocity = new Vector3(0.0f, rb.velocity.y, 0f);
+        }
+
+        //Jump cap so you cant infinitely hold jump to get an insane jump height
+        if(jumpValue >= 200f && isGrounded)
+        {
+            isJump = 0;
+            float tempx = moveHorizontal * speed;
+            float tempy = realjumpValue;
+            rb.velocity = new Vector3(tempx, tempy, 0f);
+            Invoke("ResetJump", 0.0f);
+        }
+
+        //When you release jump
+        if(Input.GetButtonUp("Jump"))
+        {    
+            isJump = 0;
+            //Lets you jump if you're grounded based on the jump value which was increased while holding space
+            if (isGrounded)
+            {
+                rb.velocity = new Vector3(moveHorizontal * speed, realjumpValue, 0f);
+                jumpValue = 0.0f;
+            }
+            canJump = true;
+        }
+    }
+    //Fixed update to check if player is grounded and to affect player's gravity scale
+    void FixedUpdate()
+    {
+        //Uses a raycast downward to check if the player is grounded or not
+        if(!Physics.Raycast(transform.position, -Vector3.up, distanceGround + 0.1f))
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+
+        //Modifies the players gravity
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
+    }
+
+    //Function to reset jump
+    void ResetJump()
+    {      
+        canJump = false;
+        jumpValue = 0;       
     }
 }
