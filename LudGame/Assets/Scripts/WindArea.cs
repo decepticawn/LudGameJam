58ee4cdfda5767b8a1cadba9f3e3ec89397a584f
decepticawn <decepticawn@gmail.com>
2021-10-22 03:37:07 -0600

    Fan Prefab and WindArea script
    Added a WindArea script that could be used on any collider that we want to have wind, just have to add the script, change its tag, then set the strength and direction. I also added a fan prefab for a vertical fan. The particles can be changed just thought I'd put some temporary ones on there for visuals. The strength/direction can also be tweaked depending on where we want to put the fan or change its orientation.  :D

---
+++ LudGame/Assets/Scripts/WindArea.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    public float Strength;
+    public Vector3 WindDirection;
+    void OnTriggerStay(Collider col)
+    {
+        Rigidbody colRigidbody = col.GetComponent<Rigidbody>();
+        if (colRigidbody != null)
+        {
+            colRigidbody.AddForce(WindDirection * Strength);
+        }
+    }
+}
