6a69bad990f1907ebf59e4e0b1993d36ae1fd16b
decepticawn <decepticawn@gmail.com>
2021-10-23 22:27:33 -0600

    Finished Pause
    Added a pause menu with a options to teleport to different sections

--- LudGame/Assets/Scripts/Pause.cs
+++ LudGame/Assets/Scripts/Pause.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu = null, teleportLoc;
+    [SerializeField] GameObject pauseMenu = null, pauseMain, teleportLoc, player;
     bool isPaused;
     // Start is called before the first frame update
     void Start()
@@ -18,9 +18,11 @@ public class Pause : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {  
+             teleportLoc.gameObject.SetActive(false);
              isPaused = !isPaused;
              Time.timeScale = isPaused ? 0 : 1;
 		     pauseMenu.SetActive(isPaused);
+             pauseMain.gameObject.SetActive(true);
         }
         else if(Input.GetKeyDown(KeyCode.Escape)&&isPaused)
         {
@@ -29,14 +31,36 @@ public class Pause : MonoBehaviour
     }
     public void Continue()
     {
+        teleportLoc.gameObject.SetActive(false);
         isPaused = !isPaused;
         Time.timeScale = isPaused ? 0 : 1;
 		pauseMenu.SetActive(isPaused);
     }
     public void Teleport()
     {
-
+        pauseMain.gameObject.SetActive(false);
+        teleportLoc.gameObject.SetActive(true);
     }
+    public void Beginning()
+    {
+        player.transform.position = new Vector3(0f,0.5f,0f);
+    }
+
+    public void FanSection()
+    {
+        player.transform.position = new Vector3(3f,61.3f,0f);
+    }
+
+    public void WindSection()
+    {
+        player.transform.position = new Vector3(15.6f, 88.5f,0f);
+    }
+
+    public void NearEnd()
+    {
+        player.transform.position = new Vector3(18.6f,111.2f,0f);
+    }
+
     public void Exit()
     {
         Time.timeScale = 1;
@@ -44,6 +68,7 @@ public class Pause : MonoBehaviour
     }
     public void Back()
     {
-
+        pauseMain.gameObject.SetActive(true);
+        teleportLoc.gameObject.SetActive(false);
     }
 }
